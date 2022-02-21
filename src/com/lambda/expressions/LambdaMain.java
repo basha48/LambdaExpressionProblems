fb106fa1bd93c588bfe73c6f7e521cdfc03470df
basha <bashamb787@gmail.com>
2022-02-21 16:30:48 +0530

    using Lambda Expressions Solved user Registration Problems

---
+++ src/com/lambda/expressions/LambdaMain.java
@@ -0,0 +1,13 @@
+package com.lambda.expressions;
+
+public class LambdaMain {
+	public static void main(String[] args) {
+		LambdaFunctions obj = new LambdaFunctions();
+		obj.validateFirstName();
+		obj.validateLastName();
+		obj.validateEmail();
+		obj.validatePhnNum();
+		obj.validatePassword();
+	}
+
+}
